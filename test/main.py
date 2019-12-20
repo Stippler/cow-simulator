602671feadc495688031ae167a5bf923a7caa430
Stippler <christian.stippel@gmail.com>
2019-12-20 12:01:41 +0100

    add test

--- test/main.py
+++
@@ -1,2 +0,0 @@
-import tensorflow as tf
-tf.test.is_gpu_available(), tf.test.gpu_device_name()
--- test/test.py
+++ test/test.py
@@ -1,16 +1,27 @@
 import unittest
 from deepcow import *
-from deepcow.entity import Agent
+from deepcow.entity import Agent, Entity, State
+from pygame.math import Vector2
+import numpy as np
 
 
-class TestCow(unittest.TestCase):
-
+class TestAgent(unittest.TestCase):
     def test_perception(self):
-        cow = Agent()
+        grass = Entity(color=(0, 255, 0))
+        grass.position = Vector2(20, 30)
+        cow = Agent(ray_count=10, ray_length=200, direction=Vector2(-1, 0))
+        cow.position = Vector2(100, 30)
+        actual_cow_state = cow.perceive([grass])
 
-
-class TestWolf:
-    pass
+        position_correct = np.allclose(actual_cow_state.position, np.array([100, 30]))
+        self.assertTrue(position_correct)
+        direction_correct = np.allclose(actual_cow_state.direction, np.array([-1, 0]))
+        self.assertTrue(direction_correct)
+        should_perception = np.zeros((10, 3))
+        should_perception[1:-1, 1] = 1
+        perception_correct = np.allclose(actual_cow_state.perception, should_perception)
+        print(actual_cow_state.perception)
+        self.assertTrue(perception_correct)
 
 
 class TestEnvironment:
