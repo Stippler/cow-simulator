b01c8128c796882759c8e42ffe88dbdf7902e6e7
Stippler <christian.stippel@gmail.com>
2019-12-23 18:26:04 +0100

    only-cow

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def train_dqn_agents(cow_model: DQNAgent,
                      epoch_length=1000,
                      episode_length=10,
                      game_length=1000,
-                     batch_size=32) -> (DQNAgent, DQNAgent):
+                     batch_size=32):
     """ training loop for the deep q agents"""
     all_rewards = []
     for epoch in range(epoch_length):
@@ -93,6 +93,60 @@ def train_dqn_agents(cow_model: DQNAgent,
     return pd.DataFrame(data=all_rewards, columns=['epoch', 'cow_reward', 'wolf_rewards'], )
 
 
+def train_cow(
+        cow_model: DQNAgent,
+        environment: Environment,
+        epoch_length=1000,
+        episode_length=10,
+        game_length=1000,
+        batch_size=32):
+    """ training loop for the deep q agents"""
+    all_rewards = []
+    for epoch in range(epoch_length):
+        print('starting epoch {}, cow exploration rate {:.2f}%'.format(
+            epoch,
+            cow_model.get_exploration_rate()))
+
+        cow_reward_per_epoch = 0
+        cow_border_collision = 0
+
+        for episode in range(episode_length):
+            states = environment.reset()
+            cow_state = states[0]
+
+            for frame in range(game_length):
+                cow_action = cow_model.explore_select_action(cow_state)
+
+                states, rewards, done, info = environment.step([Action(cow_action)])
+
+                cow_border_collision += info['cow_border_collisions']
+
+                cow_reward = rewards[0]
+                cow_reward_per_epoch += cow_reward
+                cow_next_state = states[0]
+
+                if frame == game_length - 1:
+                    done = True
+
+                cow_model.remember(cow_state, cow_action, cow_reward_per_epoch, cow_next_state, done)
+
+                cow_state = cow_next_state
+
+                if environment.quit():
+                    return pd.DataFrame(data=all_rewards,
+                                        columns=['epoch', 'cow_reward', 'wolf_reward',
+                                                 'wolf_border_collision', 'cow_border_collision'], )
+                if done:
+                    break
+
+        all_rewards.append([epoch, cow_reward_per_epoch / episode_length,
+                            cow_border_collision / episode_length])
+
+        cow_model.replay(batch_size)
+        print('finish epoch {}, cow rewards {}'.format(epoch, cow_reward_per_epoch))
+    return pd.DataFrame(data=all_rewards, columns=['epoch', 'cow_reward', 'wolf_rewards'], )
+
+
 epoch_length = 1000
 ray_count = 20
 action_size = 7
@@ -106,15 +160,30 @@ action_size = 7
 
 cow_model = ExtendedDQNAgent(perception_size=ray_count * 3, metadata_size=3, action_size=action_size,
                              preprocess=transform_state_extended,
-                             memory_length=10_000)
+                             memory_length=100_000)
 wolf_model = ExtendedDQNAgent(perception_size=ray_count * 3, metadata_size=3, action_size=action_size,
                               preprocess=transform_state_extended,
-                              memory_length=10_000)
+                              memory_length=100_000)
 
 environment = Environment(cow_ray_count=ray_count,
                           grass_count=1,
                           wolf_ray_count=ray_count,
+                          wolf_count=0,
                           draw=True)
+
+results = train_cow(cow_model, environment, game_length=1000)
+sns.lineplot(results['epoch'], results['cow_border_collision'], color='brown', )
+sns.lineplot(results['epoch'], results['wolf_border_collision'], color='blue')
+plt.xlabel('epoch')
+plt.ylabel('border collision count')
+plt.savefig('result/cow-collision-count.png')
+plt.show()
+sns.lineplot(results['epoch'], results['cow_reward'], color='brown', )
+sns.lineplot(results['epoch'], results['wolf_reward'], color='blue')
+plt.xlabel('epoch')
+plt.ylabel('reward')
+plt.savefig('result/cow-reward.png')
+plt.show()
 
 results = train_dqn_agents(cow_model, wolf_model, environment, game_length=1000)
 print(results)
